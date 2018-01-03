038821ffd249cb5458bdfc0bb9f60465256726cd
Thomas Sojka <thomas.sojka@comsysto.com>
2018-01-03 08:16:50 +0100

    elc: Create timespans in elcontext

--- defuns/elcontext.el
+++ defuns/elcontext.el
@@ -16,11 +16,12 @@
           (lon (buffer-substring 32 41)))
       (ht (:lat (string-to-number lat)) (:lon (string-to-number lon))))))
 
+
 (defun elc--gps-to-string (gps)
   "Convert GPS coordinate in a readable format."
   (let ((lat (ht-get gps :lat))
         (lon (ht-get gps :lon)))
-    (concat "Lat: " (number-to-string lat) " Lon: " (number-to-string) lon)))
+    (concat "Lat: " (number-to-string lat) " Lon: " (number-to-string lon))))
 
 (defun elc--distance (from to)
   "Comutes the distance between FROM and TO in km."
@@ -72,28 +73,106 @@
   (interactive)
   (progn
     (setq elc--context-name (read-from-minibuffer "Name: "))
-    (defhydra hydra-context (:hint nil)
+    (defhydra hydra-context (:hint nil
+                                   :foreign-keys run
+                                   :post (progn (setq elc--context-name "")
+                                                (setq elc--context-location "")
+                                                (setq elc--context-time "")
+                                                (setq elc--context-action "")))
       "
 _n_: Change name     | Name     %`elc--context-name
 _l_: Change location | Location %`elc--context-location
 _t_: Change time     | Time     %`elc--context-time
 _a_: Change action   | Action   %`elc--context-action
 "
-      ("n" (lambda ()
-             (interactive)
-             (setq elc--context-name (read-from-minibuffer "Name: "))))
-      ("l" (lambda ()
-             (interactive)
-             (setq elc--context-location (elc--gps-to-string (elc--get-gps)))))
-      ("t" (lambda ()
-             (interactive)
-             (setq elc--context-time (read-from-minibuffer "Time: "))))
-      ("a" (lambda ()
-             (interactive)
-             (setq elc--context-action (read-from-minibuffer "Action: ")))))
+      ("n" (setq elc--context-name (read-from-minibuffer "Name: ")))
+      ("l" (setq elc--context-location (elc--gps-to-string (elc--get-gps))))
+      ("t" (elc-create-timespan) :exit t)
+      ("a" (setq elc--context-action (read-from-minibuffer "Action: ")))
+      ("c" (message "done") :color blue))
     (hydra-context/body)))
 
+(defvar elc-from-hour "")
+(defvar elc-from-minute "")
+(defvar elc-to-hour "")
+(defvar elc-to-minute "")
+(defvar elc-days ())
 
+(defhydra hydra-timespan (:hint nil :foreign-keys warn)
+  "
+_f_: Change from | From %`elc-from-hour : %`elc-from-minute
+_t_: Change to   | To   %`elc-to-hour : %`elc-to-minute
+_d_: Add days    | Days %`elc-days
+_r_: Remove days
+
+_c_: Create timespan
+"
+  ("f" (progn
+         (setq elc-from-hour (s-pad-left 2 "0" (elc-read-hour)))
+         (setq elc-from-minute (s-pad-left 2 "0" (elc-read-minute)))))
+  ("t" (progn
+         (setq elc-to-hour (s-pad-left 2 "0" (elc-read-hour)))
+         (setq elc-to-minute (s-pad-left 2 "0" (elc-read-minute)))))
+  ("d" (setq elc-days (-snoc elc-days (elc-read-week-days))))
+  ("r" (setq elc-days (-remove-item (ivy-read "Remove day:" elc-days) elc-days)))
+  ("c" (progn
+         (if (or (and (s-present? elc-from-hour) (s-blank? elc-to-hour))
+                 (and (s-present? elc-to-hour) (s-blank? elc-from-hour)))
+             (progn
+               (message "Please specify a from and to time.")
+               (hydra-timespan/body))
+           (progn
+             (setq elc--context-time (elc-timespan-to-string))
+             (progn (setq elc-from-hour "")
+                    (setq elc-from-minute "")
+                    (setq elc-to-hour "")
+                    (setq elc-to-minute "")
+                    (setq elc-days ()))
+             (hydra-context/body))))
+   :color blue))
+
+(defun elc-timespan-to-string ()
+  "Format a timespan to a string."
+  (concat
+   (if (s-present? elc-from-hour)
+       (concat "From " elc-from-hour ":" elc-from-minute
+               " To " elc-to-hour ":" elc-to-minute " ")
+     "")
+   (if (not (null elc-days))
+       (concat "On " (s-replace " " ", " (s-replace ")" "" (s-replace "(" "" (format "%s" elc-days)))))
+     "")))
+
+(defun elc-create-timespan ()
+  "Create a new timespan from user input."
+  (interactive)
+  (hydra-timespan/body))
+
+(defun elc-read-number-range (from to prompt)
+  "Read a number range between FROM and TO with a PROMPT."
+  (let ((number))
+    (while (not number)
+      (let ((userInput (read-from-minibuffer prompt)))
+        (if (and
+             (s-numeric? userInput)
+             (<= from (string-to-number userInput))
+             (>= to (string-to-number userInput)))
+            (setq number userInput)
+          (read-from-minibuffer (format "Please specify a number between %d-%d." from to)))))
+    number))
+
+(defun elc-read-week-days ()
+  "Read week days from user input."
+  (ivy-read "Week day: "
+            (-difference '("Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun" ) elc-days)
+            :require-match t))
+
+(defun elc-read-hour ()
+  "Read an hour form user input."
+  (elc-read-number-range 0 23 "Hour: "))
+
+(defun elc-read-minute ()
+  "Read an minute form user input."
+  (elc-read-number-range 0 59 "Minute: "))
 
 (provide 'elcontext)
 
--- defuns/timespan.el
+++ defuns/timespan.el
@@ -46,65 +46,6 @@
      ((and (member day days) (> hour fromHour) (< hour toHour)) t)
      ((and (member day days) (>= hour fromHour) (>= minute fromMinute) (<= hour toHour) (<= minute toMinute)) t))))
 
-(setq ts-from-hour "")
-(setq ts-from-minute "")
-(setq ts-to-hour "")
-(setq ts-to-minute "")
-(setq ts-days ())
-(defun ts-create-timespan ()
-  "Create a new timespan from user input."
-  (interactive)
-  (progn
-    (defhydra hydra-timespan (:hint nil)
-      "
-_f_: Change from | From %`ts-from-hour : %`ts-from-minute
-_t_: Change to   | To   %`ts-to-hour : %`ts-to-minute
-_d_: Add days    | Days %`ts-days
-_r_: Remove days
-"
-      ("f" (lambda ()
-             (interactive)
-             (setq ts-from-hour (ts-read-hour))
-             (setq ts-from-minute (ts-read-minute))))
-      ("t" (lambda ()
-             (interactive)
-             (setq ts-to-hour (ts-read-hour))
-             (setq ts-to-minute (ts-read-minute))))
-      ("d" (lambda ()
-             (interactive)
-             (setq ts-days (-snoc ts-days (ts-read-week-days)))))
-      ("r" (lambda ()
-             (interactive)
-             (setq ts-days (-remove-item (ivy-read "Remove day:" ts-days) ts-days)))))
-    (hydra-timespan/body)))
-
-(defun ts-read-number-range (from to prompt)
-  "Read a number range between FROM and TO with a PROMPT."
-  (let ((number))
-    (while (not number)
-      (let ((userInput (read-from-minibuffer prompt)))
-        (if (and
-             (s-numeric? userInput)
-             (<= from (string-to-number userInput))
-             (>= to (string-to-number userInput)))
-            (setq number (string-to-number userInput))
-          (read-from-minibuffer (format "Please specify a number between %d-%d." from to)))))
-    number))
-
-(defun ts-read-week-days ()
-  "Read week days from user input."
-  (ivy-read "Week day: "
-            '("Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun" )
-            :require-match t))
-
-(defun ts-read-hour ()
-  "Read an hour form user input."
-  (ts-read-number-range 0 23 "Hour: "))
-
-(defun ts-read-minute ()
-  "Read an minute form user input."
-  (ts-read-number-range 0 59 "Minute: "))
-
 (provide 'timespan)
 
 ;;; timespan.el ends here
