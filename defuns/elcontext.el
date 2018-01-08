64c984c6ebc2aede8d7ee203b655fb5662c6acb3
Thomas Sojka <thomas.sojka@comsysto.com>
2018-01-08 09:03:23 +0100

    elc: Add methods for context edit + delete

--- defuns/elcontext.el
+++ defuns/elcontext.el
@@ -4,12 +4,24 @@
 (require 'ht)
 (require 'hydra)
 (require 'elcontext-time)
+(require 'uuidgen)
 
 (defvar elc-contexts (ht))
 
+(defvar elcontext-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "c") 'elc-new-context)
+    (define-key map (kbd "e") 'elc-edit-context)
+    (define-key map (kbd "d") 'elc-delete-context)
+    map)
+  "Keymap for `elcontext-mode'.")
+
+
 (define-derived-mode elcontext-mode tabulated-list-mode "Contexts"
   "Special mode for contexts."
-  (setq tabulated-list-format [("Name" 15 t) ("Location" 30 t) ("Time" 15 t) ("Action" 20 t)])
+  (setq mode-name "elcontext")
+  (use-local-map elcontext-mode-map)
+  (setq tabulated-list-format [("Name" 15 t) ("Location" 30 t) ("Time" 30 t) ("Action" 20 t)])
   (setq tabulated-list-entries 'elc-get-contexts-for-table)
   (tabulated-list-init-header)
   (tabulated-list-print))
@@ -25,7 +37,7 @@
   "Return all context in table format."
   (ht-map (lambda (key context)
             (list key
-                  (vector key
+                  (vector (ht-get context :name)
                           (elc--gps-to-string (ht-get context :location))
                           (elc-time-timespan-to-string (ht-get context :time))
                           (format "%s" (ht-get context :action)))))
@@ -47,33 +59,47 @@
                      (eval (ht-get context :action)))))
              elc-contexts)))
 
-(setq elc--context-current-name "")
-(setq elc--context-current (ht (:time (ht)) (:action nil) (:location nil)))
+(setq elc--context-id nil)
+(setq elc--context-current (ht (:name nil) (:time (ht)) (:action nil) (:location nil)))
 
 (defhydra hydra-context (:hint nil :foreign-keys warn)
       "
-_n_: Change name     | Name     %`elc--context-current-name
+_n_: Change name     | Name     %(ht-get elc--context-current :name)
 _l_: Change location | Location %(elc-location-gps-to-string (ht-get elc--context-current :location))
 _t_: Change time     | Time     %(elc-time-timespan-to-string (ht-get elc--context-current :time))
 _a_: Change action   | Action   %(ht-get elc--context-current :action)
 
 _c_: Create context
 "
-      ("n" (setq elc--context-current-name (read-from-minibuffer "Name: ")))
+      ("n" (ht-set! elc--context-current :name (read-from-minibuffer "Name: ")))
       ("l" (ht-set! elc--context-current :location (elc-location-get-gps)))
       ("t" (elc-time-create-timespan (ht-get elc--context-current :time)) :exit t)
       ("a" (ht-set! elc--context-current :action (read-minibuffer "Action: ")))
       ("c" (progn
-             (elc-add-context elc--context-current-name elc--context-current)
-             (setq elc--context-current-name "")
-             (setq elc--context-current (ht (:time (ht)) (:action nil) (:location nil))))
+             (elc-add-context elc--context-id elc--context-current)
+             (setq elc--context-current (ht (:name nil) (:time (ht)) (:action nil) (:location nil)))
+             (tabulated-list-print))
        :color blue))
 
 (defun elc-new-context ()
   "Create a new context."
   (interactive)
+  (setq elc--context-id (uuidgen-4))
   (hydra-context/body))
 
+(defun elc-edit-context ()
+  "Edit context at point."
+  (interactive)
+  (let ((context-id (tabulated-list-get-id)))
+    (setq elc--context-id context-id)
+    (setq elc--context-current (ht-get elc-contexts context-id))
+    (hydra-context/body)))
+
+(defun elc-delete-context ()
+  "Delete context at point."
+  (interactive)
+  (ht-remove! elc-contexts (tabulated-list-get-id))
+  (tabulated-list-print))
 
 (provide 'elcontext)
 
