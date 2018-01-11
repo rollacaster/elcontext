6cd3c4f29de2e24143697d9fedfb2828acdb4eaa
Thomas Sojka <thomas.sojka@comsysto.com>
2018-01-11 07:55:28 +0100

    elc: Delete outdated test

--- defuns/elcontext-test.el
+++ defuns/elcontext-test.el
@@ -4,22 +4,6 @@
 (require 'ht)
 
 (ert-deftest elc-store-context-test ()
-  "Store a new location"
-  (setq elc-contexts
-        (ht ("Home"
-             (ht (:location
-                  (ht (:gps
-                       (ht (:lat 48.155565)
-                           (:lon 11.538645)))))))
-            ))
-  (elc-add-context "Office"
-                   (ht (:location
-                        (ht (:gps
-                             (ht (:lat 48.126011)
-                                 (:lon 11.558044)))))))
-  (should (equal (elc--get-lat "Office") 48.126011))
-  (should (equal (elc--get-lon "Office") 11.558044))
-  (should (equal (elc--get-lat "Home") 48.155565))
-  (should (equal (elc--get-lon "Home") 11.538645)))
+  "Store a new location")
 
 ;;; elcontext-test.el ends here
